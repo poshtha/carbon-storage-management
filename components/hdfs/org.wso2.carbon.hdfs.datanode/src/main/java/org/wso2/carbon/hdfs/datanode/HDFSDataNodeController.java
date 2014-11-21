48cc57841048c60c0c01d8911525311944dd1283
poshitha <poshitha@wso2.com>
2014-11-21 14:20:52 +0530

    adding datanode carbinisation

--- components/hdfs/org.wso2.carbon.hdfs.datanode/src/main/java/org/wso2/carbon/hdfs/datanode/HDFSDataNodeController.java
+++ components/hdfs/org.wso2.carbon.hdfs.datanode/src/main/java/org/wso2/carbon/hdfs/datanode/HDFSDataNodeController.java
@@ -1,21 +1,3 @@
-/*
- *  Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- *
- *  WSO2 Inc. licenses this file to you under the Apache License,
- *  Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
- *
- */
 package org.wso2.carbon.hdfs.datanode;
 
 import org.apache.commons.logging.Log;
@@ -26,45 +8,45 @@ import org.osgi.service.component.ComponentContext;
 import org.wso2.carbon.user.core.service.RealmService;
 
 /**
- * @scr.component name="org.wso2.carbon.hdfs.datanode.component" immediate="true"
- * @scr.reference name="user.realmservice.default" interface="org.wso2.carbon.user.core.service.RealmService"
- * cardinality="1..1" policy="dynamic" bind="setRealmService"  unbind="unsetRealmService"
+ * @scr.component name="org.wso2.carbon.hdfs.datanode.component"
+ *                immediate="true"
+ * @scr.reference name="user.realmservice.default"
+ *                interface="org.wso2.carbon.user.core.service.RealmService"
+ *                cardinality="1..1" policy="dynamic" bind="setRealmService"
+ *                unbind="unsetRealmService"
  */
 
 public class HDFSDataNodeController {
 
-    private static Log log = LogFactory.getLog(HDFSDataNodeController.class);
-    private RealmService realmService;
+	private static Log log = LogFactory.getLog(HDFSDataNodeController.class);
+	private RealmService realmService;
+	private static final String FALSE = "false";
 
-    protected void activate(ComponentContext componentContext) {
-    	String enableHDFSdatanode = System.getProperty("enable.hdfs.datanode");
-    	
-        if (log.isDebugEnabled()) {
-            log.debug("HDFS Data Node bunddle is activated.");
-        }
-        if (("false".equals(enableHDFSdatanode))) {
-            log.debug("HDFS data node is disabled and not started in the service activator");
-            return;
-        }
-        log.info("HDFS: HDFS Data Node is activated");
-        HDFSDataNodeComponentManager.getInstance().init(realmService);
-        new HDFSDataNode();
+	protected void activate(ComponentContext componentContext) throws Exception {
+		String enableHDFSdatanode = System.getProperty("enable.hdfs.datanode");
 
-    }
+		if ((FALSE.equals(enableHDFSdatanode))) {
+			log.debug("HDFS data node is disabled and not started in the service activator");
+			return;
+		}
+		HDFSDataNodeComponentManager.getInstance().init(realmService);
+		HDFSDataNode dataNode = new HDFSDataNode();
+		dataNode.start();
 
-    protected void deactivate(ComponentContext componentContext) {
-        if (log.isDebugEnabled()) {
-            log.debug("HDFS Data Node bunddle is deactivated.");
-        }
-        HDFSDataNodeComponentManager.getInstance().destroy();
-    }
+	}
 
-    protected void setRealmService(RealmService realmService) {
-        this.realmService = realmService;
-    }
+	protected void deactivate(ComponentContext componentContext) {
+		if (log.isDebugEnabled()) {
+			log.debug("HDFS Data Node bunddle is deactivated.");
+		}
+		HDFSDataNodeComponentManager.getInstance().destroy();
+	}
 
-    protected void unsetRealmService(RealmService realmService) {
-        this.realmService = null;
-    }
+	protected void setRealmService(RealmService realmService) {
+		this.realmService = realmService;
+	}
+
+	protected void unsetRealmService(RealmService realmService) {
+		this.realmService = null;
+	}
 }
-
--- components/hdfs/org.wso2.carbon.hdfs.namenode/src/main/java/org/wso2/carbon/hdfs/namenode/HDFSNameNodeController.java
+++ components/hdfs/org.wso2.carbon.hdfs.namenode/src/main/java/org/wso2/carbon/hdfs/namenode/HDFSNameNodeController.java
@@ -1,21 +1,3 @@
-/*
- *  Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- *
- *  WSO2 Inc. licenses this file to you under the Apache License,
- *  Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
- *
- */
 package org.wso2.carbon.hdfs.namenode;
 
 import org.apache.commons.logging.Log;
@@ -33,19 +15,20 @@ public class HDFSNameNodeController {
 
     private static Log log = LogFactory.getLog(HDFSNameNodeController.class);
     private RealmService realmService;
-
-    protected void activate(ComponentContext componentContext) {
+    private String FALSE = "false";
+    protected void activate(ComponentContext componentContext) throws Throwable {
     	String enableHDFSstartup = System.getProperty("enable.hdfs.startup");
     	
         if (log.isDebugEnabled()) {
             log.debug("HDFS Name Node bunddle is activated.");
         }
-        if (("false".equals(enableHDFSstartup))) {
+        if ((FALSE.equals(enableHDFSstartup))) {
             log.debug("HDFS name node is disabled and not started in the service activator");
             return;
         }
         HDFSNameNodeComponentManager.getInstance().init(realmService);
-        new HDFSNameNode();
+        HDFSNameNode nameNode = new HDFSNameNode();
+        nameNode.start(); // start name node
     }
 
     protected void deactivate(ComponentContext componentContext) {
